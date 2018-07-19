3146bb89419a355928f1454a86bb376892e3990e
sloanster4000 <sloanster4000@gmail.com>
2018-07-19 10:11:24 -0600

    Update f5_cert_check.bash

--- f5_cert_check.bash
+++ f5_cert_check.bash
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 #10/10/2017:    Initial version.
+#Copyright (c) 2018 Sloanster4000
+#Licensed under the terms of LICENSE included in this project
 
 ### Get list of certs and expriation date ###
 ### Format them in one line               ###
